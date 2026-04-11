dc68e2824159461aba6f402761776ae88fe0acf9
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-04-11 14:18:25 +0300

    build(debug): добавлена поддержка DEBUG_LOG в main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <string>
 
 #include "app.h"
+#include "debug_log.h"
 
 int main(int argc, char* argv[]) {
+    DEBUG_LOG("Application started");
     if (argc > 1 && std::string(argv[1]) == "--test") {
         return runTestMode();
     }
